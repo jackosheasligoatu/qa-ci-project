529ce59a6ad879467280baacff3bdf939b2a8570
jackosheasligoatu <S00275602@atu.ie>
2026-04-20 15:26:30 +0100

    Use environment variables for login credentials
    Refactor withdrawal test to use environment variables for credentials.

--- tests/test_withdrawal.py
+++ tests/test_withdrawal.py
@@ -1,5 +1,13 @@
 from playwright.sync_api import sync_playwright
 
+
+import os
+
+print("ENV:", os.getenv("APP_ENV"))
+
+USERNAME = os.getenv("APP_USERNAME")
+PASSWORD = os.getenv("APP_PASSWORD")
+
 def run_withdrawal():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
@@ -8,8 +16,8 @@ def run_withdrawal():
 
         # Login
         page.goto("https://portal5.omegasys.eu/login")
-        page.fill('input[placeholder="Username or Email"]', 'cloudwarrior')
-        page.fill('input[placeholder="Password"]', 'Omega123!')
+        page.fill('input[placeholder="Username or Email"]', APP_USERNAME)
+        page.fill('input[placeholder="Password"]', APP_PASSWORD)
         page.click('button:has-text("Submit")')
         page.wait_for_selector("text=This is a demo environment", timeout=10000)
         print("✅ Login successful")

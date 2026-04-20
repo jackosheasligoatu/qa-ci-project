29f0ac21e248c37788f7f1202cff88dffa81f76c
jackosheasligoatu <S00275602@atu.ie>
2026-04-20 15:51:10 +0100

    Change deposit test to headless mode
    Updated the deposit test to run headless and improved code structure.

--- tests/test_deposit.py
+++ tests/test_deposit.py
@@ -3,7 +3,7 @@ import time
 
 def run_deposit():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
 

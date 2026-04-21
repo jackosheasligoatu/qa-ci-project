122670fd8830de934698ee4c87030280b17096dd
jackosheasligoatu <S00275602@atu.ie>
2026-04-21 09:46:57 +0100

    Change default SPINS value to 10
    Updated default SPINS value from 5 to 10.

--- utils/config.py
+++ utils/config.py
@@ -6,7 +6,7 @@ APP_PASSWORD = os.getenv("APP_PASSWORD")
 HEADLESS = os.getenv("HEADLESS", "true").lower() == "true"
 
 GAME_ID = os.getenv("GAME_ID", "aztec_fire")
-SPINS = int(os.getenv("SPINS", "5"))
+SPINS = int(os.getenv("SPINS", "10"))
 SPIN_DELAY = float(os.getenv("SPIN_DELAY", "1"))
 
 DEPOSIT_AMOUNT = os.getenv("DEPOSIT_AMOUNT", "1000")
